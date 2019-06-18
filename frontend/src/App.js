51329556487daa6d750ea1b9720be8565242b84d
rakhraharpreet18 <47067011+rakhraharpreet18@users.noreply.github.com>
2019-06-17 21:22:34 -0400

    added Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+
+import { FacebookLoginButton } from "react-social-login-buttons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="login-form text-center">
+      <h1>
+        <span clasName="font-weight-bold">HR Scheduling System </span>
+      </h1>
+
+      <h2 className="text-center">Welcome</h2>
+
+      <FormGroup>
+        <Label> Email </Label>
+        <Input type="email" placeholder="Email" />
+      </FormGroup>
+
+      <FormGroup>
+        <Label> Password </Label>
+        <Input type="password" placeholder="Password" />
+      </FormGroup>
+
+      <Button className="btn-lg btn-dark btn-block">Login</Button>
+
+      <div className="text-center pt-3">
+        Or continue with your social account
+      </div>
+
+      <FacebookLoginButton className="mt-3 mb-3" />
+
+      <div className="text-center">
+        <a href="/sign-up">Sign up</a>
+        <span className="p-2">|</span>
+        <a href="/forgot-password">Forgot Password</a>
+      </div>
+    </Form>
   );
 }
 
