4018932e6042d3a085963b8543999ff87a6f112f
rakhraharpreet18 <47067011+rakhraharpreet18@users.noreply.github.com>
2019-06-17 21:48:15 -0400

    Move loginview to a serperate component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import { LoginView } from "./View/LoginView";
+
+class App extends React.Component {
+  render() {
+    return (
+      <LoginView></LoginView>
+    )
+  }
+}
+
+export default App;
---
+++ frontend/src/View/LoginView.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+// eslint-disable-next-line
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+// eslint-disable-next-line
+import { FacebookLoginButton } from "react-social-login-buttons";
+
+export class LoginView extends React.Component {
+    render() {
+        return (
+            <Form className="login-form text-center">
+                <h1>
+                    <span clasName="font-weight-bold">HR Scheduling System </span>
+                </h1>
+
+                <h2 className="text-center">Welcome</h2>
+
+                <FormGroup>
+                    <Label> Email </Label>
+                    <Input type="email" placeholder="Email" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label> Password </Label>
+                    <Input type="password" placeholder="Password" />
+                </FormGroup>
+
+                <Button className="btn-lg btn-dark btn-block">Login</Button>
+
+                <div className="text-center pt-3">
+                    Or continue with your social account
+            </div>
+
+                <FacebookLoginButton className="mt-3 mb-3" />
+
+                <div className="text-center">
+                    <a href="/sign-up">Sign up</a>
+                    <span className="p-2">|</span>
+                    <a href="/forgot-password">Forgot Password</a>
+                </div>
+            </Form>
+        );
+    }
+}
