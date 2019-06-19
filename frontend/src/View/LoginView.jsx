7b26484bb678806b9e91ceb8034e9b74f51daee8
rakhraharpreet18 <47067011+rakhraharpreet18@users.noreply.github.com>
2019-06-18 21:13:41 -0400

    Registration Page added with better css for Login and Register page

--- frontend/src/View/LoginView.jsx
+++ frontend/src/View/LoginView.jsx
@@ -17,38 +17,64 @@ export class LoginView extends React.Component {
 
     render() {
         return (
-            <Form className="login-form text-center">
-                <h1>
-                    <span clasName="font-weight-bold">HR Scheduling System </span>
-                </h1>
+            <div className="wrapper">
+                <div className="form-wrapper">
+                    <Form noValidate>
+                        <h1>
+                            <span clasName="font-weight-bold">HR Scheduling System </span>
+                        </h1>
 
-                <h2 className="text-center">Welcome</h2>
+                        <h2 className="text-center">Welcome</h2>
 
-                <FormGroup>
-                    <Label> Email </Label>
-                    <Input type="email" placeholder="Email" />
-                </FormGroup>
+                        <div className="email">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                //className={formErrors.email.length > 0 ? "error" : null}
+                                placeholder="Email"
+                                type="email"
+                                name="email"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                            {/* {formErrors.email.length > 0 && (
+                                <span className="errorMessage">{formErrors.email}</span>
+                            )} */}
+                        </div>
 
-                <FormGroup>
-                    <Label> Password </Label>
-                    <Input type="password" placeholder="Password" />
-                </FormGroup>
+                        <div className="password">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                //className={formErrors.password.length > 0 ? "error" : null}
+                                placeholder="Password"
+                                type="password"
+                                name="password"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                            {/* {formErrors.password.length > 0 && (
+                                <span className="errorMessage">{formErrors.password}</span>
+                            )} */}
+                        </div>
 
-                <Button onClick={() => this._loginButtonPress()} className="btn-lg btn-dark btn-block">Login</Button>
-                <Button onClick={() => this._registerButtonPress()} className="btn-lg btn-dark btn-block">Register</Button>
+                        <Button onClick={() => this._loginButtonPress()} className="btn-lg btn-dark btn-block">Login</Button>
+                        <Button onClick={() => this._registerButtonPress()} className="btn-lg btn-dark btn-block">Register</Button>
 
-                <div className="text-center pt-3">
-                    Or continue with your social account
-            </div>
+                        <div className="social text-center">
+                            Or continue with your social account
+                        </div>
 
-                <FacebookLoginButton className="mt-3 mb-3" />
+                        <div className="faceLogo">
+                            <FacebookLoginButton className="mt-3 mb-3" />
+                        </div>
 
-                <div className="text-center">
-                    <a href="/sign-up">Sign up</a>
-                    <span className="p-2">|</span>
-                    <a href="/forgot-password">Forgot Password</a>
+                        <div className="social text-center">
+                            <a href="/sign-up">Sign up</a>
+                            <span className="p-2">|</span>
+                            <a href="/forgot-password">Forgot Password</a>
+                        </div>
+                    </Form>
                 </div>
-            </Form>
+            </div>
         );
     }
 }
--- frontend/src/View/RegisterView.jsx
+++ frontend/src/View/RegisterView.jsx
@@ -17,43 +17,92 @@ export class RegisterView extends React.Component {
 
     render() {
         return (
-            <Form className="register-form text-center">
-                <h1>
-                    <span clasName="font-weight-bold">HR Scheduling System </span>
-                </h1>
+            <div className="wrapper">
+                <div className="form-wrapper">
 
-                <h2 className="text-center">Register</h2>
+                    <h1>
+                        <span clasName="font-weight-bold">HR Scheduling System </span>
+                    </h1>
+                    <Form noValidate>
+                        <h2 className="text-center">Register</h2>
+                        <div className="firstName">
+                            <label htmlFor="firstName">First Name</label>
+                            <input
+                                //className={formErrors.firstName.length > 0 ? "error" : null}
+                                placeholder="First Name"
+                                type="text"
+                                name="firstName"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                            {/* {formErrors.firstName.length > 0 && (
+                                <span className="errorMessage">{formErrors.firstName}</span>
+                            )} */}
+                        </div>
+                        <div className="lastName">
+                            <label htmlFor="lastName">Last Name</label>
+                            <input
+                                //className={formErrors.lastName.length > 0 ? "error" : null}
+                                placeholder="Last Name"
+                                type="text"
+                                name="lastName"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                            {/* {formErrors.lastName.length > 0 && (
+                                <span className="errorMessage">{formErrors.lastName}</span>
+                            )} */}
+                        </div>
 
-                <FormGroup >
-                    <Label> Full Name </Label>
-                    <Input type="text" placeholder="Full name" />
-                </FormGroup>
+                        <div className="email">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                //className={formErrors.email.length > 0 ? "error" : null}
+                                placeholder="Email"
+                                type="email"
+                                name="email"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                            {/* {formErrors.email.length > 0 && (
+                                <span className="errorMessage">{formErrors.email}</span>
+                            )} */}
+                        </div>
 
-                <FormGroup>
-                    <Label> Email </Label>
-                    <Input type="email" placeholder="Email" />
-                </FormGroup>
 
-                <FormGroup>
-                    <Label> Password </Label>
-                    <Input type="password" placeholder="Password" />
-                </FormGroup>
+                        <div className="password">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                //className={formErrors.password.length > 0 ? "error" : null}
+                                placeholder="Password"
+                                type="password"
+                                name="password"
+                                noValidate
+                                onChange={this.handleChange}
+                            />
+                            {/* {formErrors.password.length > 0 && (
+                                <span className="errorMessage">{formErrors.password}</span>
+                            )} */}
+                        </div>
 
-                <Button onClick={() => this._loginButtonPress()} className="btn-lg btn-dark btn-block">Login</Button>
-                <Button onClick={() => this._registerButtonPress()} className="btn-lg btn-dark btn-block">Register</Button>
+                        <Button onClick={() => this._loginButtonPress()} className="btn-lg btn-dark btn-block">Login</Button>
+                        <Button onClick={() => this._registerButtonPress()} className="btn-lg btn-dark btn-block">Register</Button>
 
-                <div className="text-center pt-3">
-                    Or continue with your social account
-            </div>
+                        <div className="social">
+                            Or continue with your social account
+                        </div>
 
-                <FacebookLoginButton className="mt-3 mb-3" />
-
-                <div className="text-center">
-                    <a href="/sign-up">Sign up</a>
-                    <span className="p-2">|</span>
-                    <a href="/forgot-password">Forgot Password</a>
+                        <div className="faceLogo">
+                            <FacebookLoginButton className="mt-3 mb-3" />
+                        </div>
+                        <div className="social">
+                            <a href="/sign-up">Sign up</a>
+                            <span className="p-2">|</span>
+                            <a href="/forgot-password">Forgot Password</a>
+                        </div>
+                    </Form>
                 </div>
-            </Form>
+            </div>
         );
     }
 }
