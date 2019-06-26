69d0886343953e64f60f9267091f372d8bf2e672
Steven <stevennhnk113@gmail.com>
2019-06-26 12:09:21 -0700

    added vacation class

---
+++ BackEnd/src/main/java/com/Group6/ScheduleMe/Entities/Vacation.java
@@ -0,0 +1,63 @@
+package com.Group6.ScheduleMe.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "vacations")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, 
+        allowGetters = true)
+public class Vacation {
+    @NotBlank
+    private Date StartTime;
+    
+    @NotBlank
+    private Date EndTime;
+
+	@NotBlank
+	private Long ScheduledFor;
+    
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+	public Date getStartTime() {
+		return StartTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		StartTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return EndTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		EndTime = endTime;
+	}
+
+	public Long getScheduledFor() {
+		return ScheduledFor;
+	}
+
+	public void setScheduledFor(Long scheduledFor) {
+		ScheduledFor = scheduledFor;
+	}
+    
+    
+}
