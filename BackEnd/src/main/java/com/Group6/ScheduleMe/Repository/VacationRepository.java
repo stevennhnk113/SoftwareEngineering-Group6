83db1b053b46ea019fe4a17a1bb7566adb5fc952
Steven <stevennhnk113@gmail.com>
2019-07-02 20:29:48 -0700

    Added VacationRepository

---
+++ BackEnd/src/main/java/com/Group6/ScheduleMe/Repository/VacationRepository.java
@@ -0,0 +1,17 @@
+package com.Group6.ScheduleMe.Repository;
+
+import com.Group6.ScheduleMe.Entities.Vacation;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VacationRepository extends JpaRepository<Vacation, Long>{
+	
+	@Query(value = "SELECT u FROM Vacation u WHERE u.ScheduleFor = ?1", nativeQuery = false)
+	Optional<List<Vacation>> findByScheduledFor(Long id);
+}
