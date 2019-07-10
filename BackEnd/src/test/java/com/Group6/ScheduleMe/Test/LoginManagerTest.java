4dc08ed1dabda95006446a95b980973146c47fe7
komal8693 <komalk@student.douglascollege.ca>
2019-07-09 21:29:34 -0700

    LoginManagerTest class is modified

--- BackEnd/src/test/java/com/Group6/ScheduleMe/Test/LoginManagerTest.java
+++ BackEnd/src/test/java/com/Group6/ScheduleMe/Test/LoginManagerTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -30,27 +30,38 @@ public class LoginManagerTest {
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
-  @After
-  public void tearDown() {
-    driver.quit();
+
+ @After
+ public void tearDown() {
+   driver.quit();
+
   }
-  @Test
-  public void loginManager() {
-    driver.get("http://localhost:3000/");
-    driver.manage().window().setSize(new Dimension(1050, 708));
-    driver.findElement(By.name("Username")).click();
-    driver.findElement(By.name("Username")).sendKeys("steve");
-    driver.findElement(By.name("password")).click();
-    driver.findElement(By.name("password")).sendKeys("33");
-    driver.findElement(By.cssSelector(".btn-lg:nth-child(5)")).click();
-    driver.findElement(By.cssSelector(".dropdown-toggle")).click();
-    driver.findElement(By.cssSelector(".dropdown-item:nth-child(2)")).click();
-    driver.findElement(By.cssSelector(".rbc-btn-group:nth-child(3) > button:nth-child(1)")).click();
-    driver.findElement(By.cssSelector(".rbc-btn-group:nth-child(3) > button:nth-child(2)")).click();
-    driver.findElement(By.cssSelector(".dropdown-toggle")).click();
-    driver.findElement(By.cssSelector(".dropdown-item:nth-child(1)")).click();
-    driver.findElement(By.linkText("My Profile")).click();
-    driver.findElement(By.cssSelector(".wrapper")).click();
-    driver.findElement(By.linkText("Log out")).click();
+
+@Test
+public void loginManager() {
+  driver.get("http://localhost:3000/");
+  driver.manage().window().setSize(new Dimension(1050, 708));
+  driver.findElement(By.name("Username")).click();
+  driver.findElement(By.name("Username")).sendKeys("steve");
+  driver.findElement(By.name("password")).click();
+  driver.findElement(By.name("password")).sendKeys("33");
+  driver.findElement(By.cssSelector(".btn-lg:nth-child(5)")).click();
+  
+  driver.findElement(By.linkText("My Profile")).click();
+  {
+    WebElement element = driver.findElement(By.linkText("My Profile"));
+    Actions builder = new Actions(driver);
+    builder.moveToElement(element).perform();
   }
+  {
+    WebElement element = driver.findElement(By.tagName("body"));
+    Actions builder = new Actions(driver);
+    builder.moveToElement(element, 0, 0).perform();
+  }
+  driver.findElement(By.linkText("Log out")).click();
 }
+}
+
+
+
+
--- BackEnd/src/test/java/com/Group6/ScheduleMe/Test/LoginUserTest.java
+++ BackEnd/src/test/java/com/Group6/ScheduleMe/Test/LoginUserTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
