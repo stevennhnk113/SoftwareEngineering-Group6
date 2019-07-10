6b567c4ffcc988d51e2817740ebbf22e588653ab
komal8693 <komalk@student.douglascollege.ca>
2019-07-09 23:00:40 -0700

    UserScheduleTest class is craeted

---
+++ BackEnd/src/test/java/com/Group6/ScheduleMe/Test/UserScheduleTest.java
@@ -0,0 +1,14 @@
+package com.Group6.ScheduleMe.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserScheduleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
