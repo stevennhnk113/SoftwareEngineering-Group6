e05bbbe57af50eaba35be46b547511cf842d78aa
komal8693 <komalk@student.douglascollege.ca>
2019-07-15 12:48:11 -0700

    vacation controller code is added

---
+++ frontend/src/Controller/VacationController.js
@@ -0,0 +1,60 @@
+import { BaseController } from "./BaseController";
+
+class VacationController extends BaseController {
+	_UserId = "";
+	_User = null;
+
+	async GetUserByID(id) {
+		var restApi = "/api/user/" + id.toString();
+
+		this._User = await this.Get(restApi);
+		console.log(this._User);
+		return this._User;
+	}
+
+	GetUser() {
+		// console.log(this._User);
+		return this._User;
+	}
+
+	async GetAllUsers() {
+
+	}
+
+	async UserLogin(username, password) {
+		var restApi = "/api/user/login";
+
+		var payload = {
+			username: username,
+			password: password
+		};
+
+		this._User = await this.Post(restApi, payload);
+		// console.log("blad")
+		// console.log(this._User);
+		if (this._User != null) {
+			this._UserId = this._User.id;
+		}
+
+		return this._User;
+	}
+
+	async GetNonManagerUsers() {
+		var restApi = "/api/user/not/manager"
+
+		var users = await this.Get(restApi);
+		console.log(users);
+		return users;
+	}
+
+	async CreateUser(user) {
+		var restApi = "/api/user"
+
+		var users = await this.Post(restApi, user);
+		console.log(users);
+		return users;
+	}
+}
+
+var UsercontrollerObj = new UserController();
+export default UsercontrollerObj;
