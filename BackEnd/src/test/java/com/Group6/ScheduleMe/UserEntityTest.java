59dbe2247a1753e8753c2633872182c602fa8ddf
komal8693 <komalk@student.douglascollege.ca>
2019-07-23 12:54:48 -0700

    UserControllerTest class is created and user class is updated and userenitityTest class is created but not implemented yet

---
+++ BackEnd/src/test/java/com/Group6/ScheduleMe/UserEntityTest.java
@@ -0,0 +1,25 @@
+package com.Group6.ScheduleMe;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserEntityTest {
+
+	
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
