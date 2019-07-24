e567d65ce91c0b77b3b686b6a949623534cd2561
komal8693 <komalk@student.douglascollege.ca>
2019-07-23 22:23:06 -0700

    UserEntityTest class is implemented using junit white box testing

--- BackEnd/src/test/java/com/Group6/ScheduleMe/UserEntityTest.java
+++ BackEnd/src/test/java/com/Group6/ScheduleMe/UserEntityTest.java
@@ -8,6 +8,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.Group6.ScheduleMe.Entities.User;
+
 public class UserEntityTest {
 
 	
@@ -18,8 +20,52 @@ public class UserEntityTest {
 
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setFirstNameTest()  {
+		User user = new User();
+		user.setFirstName("John");
+		assertEquals(user.getFirstName(),"John");
+		
 	}
 
+	@Test
+	public void getFirstNameTest()  {
+		User user = new User();
+		user.setFirstName("John");
+		assertEquals("John",user.getFirstName());
+		
+	}
+	
+	@Test
+	public void setLastNameTest()  {
+		User user = new User();
+		user.setLastName("John");
+		assertEquals(user.getLastName(),"John");
+		
+	}
+
+	@Test
+	public void getLastNameTest()  {
+		User user = new User();
+		user.setLastName("John");
+		assertEquals("John",user.getLastName());
+		
+	}
+	
+	@Test
+	public void setUserNameTest()  {
+		User user = new User();
+		user.setUserName("John123");
+		assertEquals(user.getUserName(),"John123");
+		
+	}
+
+	@Test
+	public void getUserNameeTest()  {
+		User user = new User();
+		user.setUserName("John123");
+		assertEquals("John123",user.getUserName());
+		
+	}
+	
+	 
 }
